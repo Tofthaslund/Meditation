0ee31fce524ef8ed62549ba2b2c9d602ae79463f
Lasse Haslund <tofthaslund@gmail.com>
2021-04-20 16:51:37 +0100

    implemeted first steps in JS (SOUNDS/DISPLAY/DURATION)

---
+++ script.js
@@ -0,0 +1,18 @@
+const app = () => {
+
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get the lenght of the outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    let fakeDuration = 600;
+};
+
+app();
